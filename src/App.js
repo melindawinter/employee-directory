9e3512822334e5155c78c0049425fb984f30264a
Melinda Winter <melindawinter42@gmail.com>
2020-09-23 16:29:25 -0700

    create Main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Main from "./views/main/Main.js";
 
 function App() {
   return (
   <div className="App">
-
+    <Main />
 
   </div>
   );
---
+++ src/views/main/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Main.css";
+import Table from "../components/Table.js";
+
+export default class Main extends Component {
+    render() {
+        return (
+            <Table/>
+        );
+    }
+}

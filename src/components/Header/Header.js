5bc86f8de07152799083f8425148d7b572d2c0e1
Melinda Winter <melindawinter42@gmail.com>
2020-09-24 17:25:01 -0700

    build header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <h3>Filter employees by category or use the search box</h3>
+    </div>
+  );
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.css";
+import DataArea from "../DataArea/DataArea.js";
+
+function Main() {
+  return (
+    <>
+      <DataArea />
+    </>
+  );
+}

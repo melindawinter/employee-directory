3f41f571be40dac2ce170f7b3dd3b144a627486a
Melinda Winter <melindawinter42@gmail.com>
2020-09-25 13:12:22 -0700

    create DataAreaContext

--- src/components/DataArea/DataArea.js
+++ src/components/DataArea/DataArea.js
@@ -1,69 +1,73 @@
-import React, { Component } from "react";
+
+import React, { useState, useEffect } from "react";
 import "./DataArea.css";
-import Table from "./Table/Table.js";
+import DataTable from "./DataTable/DataTable.js";
+import "../Nav/Nav.js";
+import API from "../../utils/API.js";
+import DataAreaContext from "../../utils/DataAreaContext.js";
 
-export default class DataArea extends Component {
-    constructor() {
-        super();
-        this.state = {
-            employees: [{}],
-            sortedEmployees: [{}],
-            order: "descend",
-            headings: [
-                { name: "Image", width: "10%" },
-                { name: "Name", width: "10%" },
-                { name: "Phone", width: "20%" },
-                { name: "Email", width: "20%" },
-                { name: "DOB", width: "10%" }
-              ],
-              handleSort: heading => {
-                if (this.state.order === "descend") {
-                  this.setState({
-                    order: "ascend"
-                  })
-                } else {
-                  this.setState({
-                    order: "descend"
-                  })
-                }
-                const compareFnc = (a, b) => {
-                  if (this.state.order === "ascend") {
-                    // account for missing values
-                    if (a[heading] === undefined) {
-                      return 1;
-                    } else if (b[heading] === undefined) {
-                      return -1;
-                    }
-                    // numerically
-                    else if (heading === "name") {
-                      return a[heading].first.localeCompare(b[heading].first);
-                    } else {
-                      return a[heading] - b[heading];
-                    }
-                  } else {
-                    // account for missing values
-                    if (a[heading] === undefined) {
-                      return 1;
-                    } else if (b[heading] === undefined) {
-                      return -1;
-                    }
-                    // numerically
-                    else if (heading === "name") {
-                      return b[heading].first.localeCompare(a[heading].first);
-                    } else {
-                      return b[heading] - a[heading];
-                    }
-                  }
-                }
-                const sortedEmployees = this.state.filteredEmployees.sort(compareFnc);
-                this.setState({ filteredEmployees: sortedEmployees });
-              }
+
+const DataArea = () => {
+  constructor() {
+    super();
+    this.state = {
+      employees: [{}],
+      sortedEmployees: [{}],
+      order: "descend",
+      headings: [
+        { name: "Image", width: "10%" },
+        { name: "Name", width: "10%" },
+        { name: "Phone", width: "20%" },
+        { name: "Email", width: "20%" },
+        { name: "DOB", width: "10%" }
+      ],
+      handleSort: heading => {
+        if (this.state.order === "descend") {
+          this.setState({
+            order: "ascend"
+          });
+        } else {
+          this.setState({
+            order: "descend"
+          });
         }
+        const compareFnc = (a, b) => {
+          if (this.state.order === "ascend") {
+            // account for missing values
+            if (a[heading] === undefined) {
+              return 1;
+            } else if (b[heading] === undefined) {
+              return -1;
+            }
+            // numerically
+            else if (heading === "name") {
+              return a[heading].first.localeCompare(b[heading].first);
+            } else {
+              return a[heading] - b[heading];
+            }
+          } else {
+            // account for missing values
+            if (a[heading] === undefined) {
+              return 1;
+            } else if (b[heading] === undefined) {
+              return -1;
+            }
+            // numerically
+            else if (heading === "name") {
+              return b[heading].first.localeCompare(a[heading].first);
+            } else {
+              return b[heading] - a[heading];
+            }
+          }
+        };
+        const sortedEmployees = this.state.filteredEmployees.sort(compareFnc);
+        this.setState({ filteredEmployees: sortedEmployees });
+      }
+    };
+  }
 
-    }
-
-// componentDidMount API.getEmployees 
-componentDidMount() {
+  // componentDidMount API.getEmployees
+  componentDidMount() {
     API.getEmployees().then(results => {
       this.setState({
         Employees: results.data.results,
@@ -73,13 +77,13 @@ componentDidMount() {
     });
   }
 
-    render() {
-        return (
-            <Table
-            headings={this.state.headings}
-            employees={this.state.sortedEmployees}
-            handleSort={this.state.handleSort}
-            />
-        );
-    }
+  render() {
+    return (
+      <Table
+        headings={this.state.headings}
+        employees={this.state.sortedEmployees}
+        handleSort={this.state.handleSort}
+      />
+    );
+  }
 }

5ce638c40fb2839dec0cecc91745dffa7680207f
Melinda Winter <melindawinter42@gmail.com>
2020-09-25 13:46:27 -0700

    set up data area

--- src/components/DataArea/DataArea.js
+++ src/components/DataArea/DataArea.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import "./DataArea.css";
 import DataTable from "./DataTable/DataTable.js";
@@ -6,84 +5,115 @@ import "../Nav/Nav.js";
 import API from "../../utils/API.js";
 import DataAreaContext from "../../utils/DataAreaContext.js";
 
-
 const DataArea = () => {
-const [developerState, setDeveloperState] = useState({
-      employees: [],
-      filteredEmployees: [],
-      order: "descend",
-      headings: [
-        { name: "Image", width: "10%", order: "descend" },
-        { name: "Name", width: "10%", order: "descend" }, 
-        { name: "Phone", width: "20%", order: "descend" },
-        { name: "Email", width: "20%", order: "descend" },
-        { name: "DOB", width: "10%", order: "descend" }
-      ],
-     
-     
-      handleSort: heading => {
-        if (this.state.order === "descend") {
-          this.setState({
-            order: "ascend"
-          });
+  // Set up headings and display order
+  const [developerState, setDeveloperState] = useState({
+    employees: [],
+    filteredEmployees: [],
+    order: "descend",
+    headings: [
+      { name: "Image", width: "10%", order: "descend" },
+      { name: "Name", width: "10%", order: "descend" },
+      { name: "Phone", width: "20%", order: "descend" },
+      { name: "Email", width: "20%", order: "descend" },
+      { name: "DOB", width: "10%", order: "descend" }
+    ]
+  });
+
+  //  Handles the change from descending to ascending and vice versa
+  const handleSort = heading => {
+    let currentOrder = developerState.headings
+      .filter(elem => elem.name === heading)
+      .map(elem => elem.order)
+      .toString();
+
+    if (currentOrder === "descend") {
+      currentOrder = "ascend";
+    } else {
+      currentOrder = "descend";
+    }
+
+    const compareFnc = (a, b) => {
+      if (currentOrder === "ascend") {
+        // Account for missing values
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        } else if (heading === "name") {
+          return a[heading].first.localeCompare(b[heading].first);
+        } else if (heading === "dob") {
+          return a[heading].age - b[heading].age;
         } else {
-          this.setState({
-            order: "descend"
-          });
+          return a[heading].localeCompare(b[heading]);
         }
-        const compareFnc = (a, b) => {
-          if (this.state.order === "ascend") {
-            // account for missing values
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            }
-            // numerically
-            else if (heading === "name") {
-              return a[heading].first.localeCompare(b[heading].first);
-            } else {
-              return a[heading] - b[heading];
-            }
-          } else {
-            // account for missing values
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            }
-            // numerically
-            else if (heading === "name") {
-              return b[heading].first.localeCompare(a[heading].first);
-            } else {
-              return b[heading] - a[heading];
-            }
-          }
-        };
-        const sortedEmployees = this.state.filteredEmployees.sort(compareFnc);
-        this.setState({ filteredEmployees: sortedEmployees });
+      } else {
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        } else if (heading === "name") {
+          return b[heading].first.localeCompare(a[heading].first);
+        } else if (heading === "dob") {
+          return b[heading].age - a[heading].age;
+        } else {
+          return b[heading].localeCompare(a[heading]);
+        }
       }
     };
-  }
+    // Set variables for employees that have been sorted and reversed headings
+    const sortedEmployees = developerState.filteredEmployees.sort(compareFnc);
+    const updatedHeadings = developerState.headings.map(elem => {
+      elem.order = elem.name === heading ? currentOrder : elem.order;
+      return elem;
+    });
 
-  // componentDidMount API.getEmployees
-  componentDidMount() {
+    setDeveloperState({
+      ...developerState,
+      filteredEmployees: sortedEmployees,
+      headings: updatedHeadings
+    });
+  };
+
+  const handleSearchChange = event => {
+    const filter = event.target.value;
+    const filteredList = developerState.employees.filter(item => {
+      let values =
+        item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
+      console.log(filter, values);
+      if (values.indexOf(filter.toLowerCase()) !== -1) {
+        return item;
+      }
+    });
+
+    setDeveloperState({
+      ...developerState,
+      filteredEmployees: filteredList
+    });
+  };
+
+  // Allows the API to be used
+  useEffect(() => {
     API.getEmployees().then(results => {
-      this.setState({
-        Employees: results.data.results,
+      setDeveloperState({
+        ...developerState,
+        employees: results.data.results,
         filteredEmployees: results.data.results
       });
-      console.log(this.state.filteredEmployees);
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <Table
-        headings={this.state.headings}
-        employees={this.state.sortedEmployees}
-        handleSort={this.state.handleSort}
-      />
-    );
-  }
-}
+  return (
+    // Provider makes Redux available
+    <DataAreaContext.Provider
+      value={{ developerState, handleSearchChange, handleSort }}
+    >
+      <Nav />
+      <div className="data-area">
+        {developerState.filteredUsers.length > 0 ? <DataTable /> : <div></div>}
+      </div>
+    </DataAreaContext.Provider>
+  );
+};
+
+export default DataArea;
